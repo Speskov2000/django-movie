b6b0c8d927516136e841b5a15c6e71be1b3b6ebd
Peskov Sergey <serpesgeikov@gmail.com>
2022-01-17 17:31:48 +0600

    html blocks, listview, detailview, reviews

--- movies/views.py
+++ movies/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
+from .forms import ReviewForm
 
 
-class MovieView(View):
+class MovieView(ListView):
     '''Список фильмов'''
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(
-                request,
-                'movies/movies.html',
-                {'movie_list': movies}
-                )
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movies.html'
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     '''Полное описание фильмов'''
 
-    def get(self, request, pk):
+    model = Movie
+    slug_field = 'url'
+
+
+class AddReview(View):
+    '''Отзывы'''
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
         movie = Movie.objects.get(id=pk)
-        return render(request, 'movies/movie_detail.html', {'movie': movie})
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
