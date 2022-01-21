c9ce3a34495918c02bcbfa80962edfab1e95a10f
Peskov Sergey <serpesgeikov@gmail.com>
2022-01-21 18:23:29 +0600

    Use ajax

--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from django.http import JsonResponse
 
-from .models import Movie
+from .models import Movie, Actor, Genre
 from .forms import ReviewForm
 
 
-class MovieView(ListView):
+class GenreYear():
+    '''Жанры и года выхода фильмов'''
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenreYear, ListView):
     '''Список фильмов'''
 
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    # template_name = 'movies/movies.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     '''Полное описание фильмов'''
 
     model = Movie
@@ -34,3 +45,37 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    '''Вывод инфомрации о актере'''
+
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    '''Фильтр фильмов'''
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist('year')) |
+                Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
+
+
+class JsonFilterMoviesView(ListView):
+    '''Фильтр фильмов в json'''
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist('year')) |
+                Q(genres__in=self.request.GET.getlist('genre'))
+        ).distinct().values('title', 'tagline', 'url', 'poster')
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = list(self.get_queryset())
+        return JsonResponse({'movies': queryset}, safe=False)
